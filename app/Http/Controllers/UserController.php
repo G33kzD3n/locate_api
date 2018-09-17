1798570bb509085a4e861264ce7a503e650b8d75
Nadeem <nadeem.09wani@gmail.com>
2018-09-17 22:21:00 +0530

    Fixes #8

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,12 +49,12 @@ class UserController extends Controller
                 "avatar"                      => (string)$user->avatar,
                 "registration_date"           => (string)$user->registered_on,
                 "cell_no"                     => (int)$user->phone_no,
-                "stop"                        => [
+                "stop"                        => $user->stop != null ? [
                     'name'       => $user->stop->name,
                     'lat'        => (double)$user->stop->lat,
                     'lng'        => (double)$user->stop->long,
                     'stop_no'    => (int)$user->stop->stops_order
-                ]
+                ] : null
         ];
     }
 }
